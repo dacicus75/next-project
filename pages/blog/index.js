dce7cfacb726e6751d06feab41f93c8d2f729e0b
dacicus75 <dacicus75@hotmail.com>
2020-12-09 13:45:08 -0500

    added blog folder

---
+++ pages/blog/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+
+function Home() {
+    return (
+        <> 
+          <Head>
+        <title>Blog </title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+    </Head>
+    <div>Welcome, Blogger</div>
+    </>
+    )
+}
+
+export default Home
