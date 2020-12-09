9ef3469a1304b7753b1787ba036c67e242c6e385
dacicus75 <dacicus75@hotmail.com>
2020-12-09 13:36:20 -0500

    added head to files

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Head from 'next/head'
+
 function Home() {
-    return <div>Welcome, You</div>
+    return (
+        <> 
+          <Head>
+        <title>Next.js website </title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+    </Head>
+    <div>Welcome, You</div>
+    </>
+    )
 }
 
 export default Home
